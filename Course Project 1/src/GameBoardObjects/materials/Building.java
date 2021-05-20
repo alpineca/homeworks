f27472bacbf3c8e23c2668bd1a450df44558f64b
alpineca <e.nikolov92@gmail.com>
2021-05-21 02:44:18 +0300

    Simple building logic implemented

---
+++ Course Project 1/src/GameBoardObjects/materials/Building.java
@@ -0,0 +1,28 @@
+package GameBoardObjects.materials;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import GameBoardObjects.Material;
+import interfaces.GameConfig;
+
+public class Building extends Material{
+	private Color color;
+	public Building(int row, int col, Color color) {
+		super(row, col, color);
+		this.color = color;
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(Graphics g) {
+		int tileX = this.col * GameConfig.tileSize; 
+		int tileY = this.row * GameConfig.tileSize;
+		
+		g.setColor(this.color);
+		g.fillRect(tileX, tileY, GameConfig.tileSize, GameConfig.tileSize);
+		g.setColor(Color.DARK_GRAY);
+		g.drawRect(tileX, tileY, GameConfig.tileSize, GameConfig.tileSize);
+		
+	}
+}
--- Course Project 1/src/game/GameBoard.java
+++ Course Project 1/src/game/GameBoard.java
@@ -1,5 +1,6 @@
 package game;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -11,6 +12,7 @@ import GameBoardObjects.ArmyUnits.Drunker;
 import GameBoardObjects.ArmyUnits.Fisherman;
 import GameBoardObjects.ArmyUnits.RockTrower;
 import GameBoardObjects.ArmyUnits.Tracktorist;
+import GameBoardObjects.materials.Building;
 import GameBoardObjects.materials.Ground;
 import interfaces.GameConfig;
 
@@ -36,6 +38,10 @@ public class GameBoard extends JPanel implements KeyListener{
 		this.gameBoard[14][12] = new RockTrower(14, 12);
 		this.gameBoard[14][13] = new Drunker(14, 13);
 		this.gameBoard[14][14] = new Fisherman(14, 14);
+		
+		this.createSmallBuilding(2, 2);
+		this.createMiddleBuilding(4, 7);
+		this.createLargeBuilding(8, 1);
 		
 	}
 	
@@ -65,6 +71,38 @@ public class GameBoard extends JPanel implements KeyListener{
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	private void createSmallBuilding(int row, int col) {
+		Color color = Color.GREEN;
+		
+		this.gameBoard[row][col] 			= new Building(row, col, color);
+		this.gameBoard[row][col + 1] 		= new Building(row, col + 1, color);
+		this.gameBoard[row + 1][col] 		= new Building(row + 1, col, color);
+		this.gameBoard[row + 1][col + 1] 	= new Building(row + 1, col + 1, color);
+	}
+	private void createMiddleBuilding(int row, int col) {
+		Color color = Color.BLUE;
+		
+		this.gameBoard[row][col] 			= new Building(row, col, color);
+		this.gameBoard[row][col + 1] 		= new Building(row, col + 1, color);
+		this.gameBoard[row][col + 2] 		= new Building(row, col + 2, color);
+		this.gameBoard[row + 1][col] 		= new Building(row + 1, col, color);
+		this.gameBoard[row + 1][col + 1] 	= new Building(row + 1, col + 1, color);
+		this.gameBoard[row + 1][col + 2] 	= new Building(row + 1, col + 2, color);
+	}
+	private void createLargeBuilding(int row, int col) {
+		Color color = Color.LIGHT_GRAY;
+		
+		this.gameBoard[row][col] 			= new Building(row, col, color);
+		this.gameBoard[row][col + 1] 		= new Building(row, col + 1, color);
+		this.gameBoard[row][col + 2] 		= new Building(row, col + 2, color);
+		this.gameBoard[row + 1][col] 		= new Building(row + 1, col, color);
+		this.gameBoard[row + 1][col + 1] 	= new Building(row + 1, col + 1, color);
+		this.gameBoard[row + 1][col + 2] 	= new Building(row + 1, col + 2, color);
+		this.gameBoard[row + 2][col] 		= new Building(row + 2, col, color);
+		this.gameBoard[row + 2][col + 1] 	= new Building(row + 2, col + 1, color);
+		this.gameBoard[row + 2][col + 2] 	= new Building(row + 2, col + 2, color);
 	}
 
 }
